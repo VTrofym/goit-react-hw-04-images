397302aed965b60e78acb7926cc1f64413a6f5d7
VTrofym <v.trof@icloud.com>
2022-10-28 16:59:10 +0300

    finished App and Searchbar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,41 +1,29 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Searchbar } from 'components/Searchbar';
 import { ImageGallery } from 'components/ImageGallery';
 import { Modal } from 'components/Modal';
 import css from './App.module.css';
 
-export class App extends Component {
-  state = {
-    savedQuery: '',
-    largeImageURL: '',
-    isOpenModal: false,
+export const App = () => {
+  const [savedQuery, setSavedQuery] = useState('');
+  const [largeImageURL, setLargeImageURL] = useState('');
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const handleSubmit = query => {
+    setSavedQuery(query);
   };
 
-  handleSubmit = query => {
-    this.setState({ savedQuery: query });
+  const toggleModal = url => {
+    setLargeImageURL(url);
   };
 
-  toggleModal = url => {
-    this.setState({
-      largeImageURL: url,
-    });
-  };
-
-  render() {
-    return (
-      <div className={css.app}>
-        <Searchbar handleSubmit={this.handleSubmit} />
-        <ImageGallery
-          toggleModal={this.toggleModal}
-          query={this.state.savedQuery}
-        />
-        {this.state.largeImageURL && (
-          <Modal
-            onClose={this.toggleModal}
-            largeImageURL={this.state.largeImageURL}
-          />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className={css.app}>
+      <Searchbar handleSubmit={handleSubmit} />
+      <ImageGallery toggleModal={toggleModal} query={savedQuery} />
+      {largeImageURL && (
+        <Modal onClose={toggleModal} largeImageURL={largeImageURL} />
+      )}
+    </div>
+  );
+};
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,45 +1,86 @@
-import { Component } from 'react';
 import css from './Searchbar.module.css';
 import PropTypes from 'prop-types';
-export class Searchbar extends Component {
-  state = {
-    query: '',
-  };
+import { useState } from 'react';
 
-  handleChange = event => {
+export const Searchbar = ({ handleSubmit }) => {
+  const [query, setQuery] = useState('');
+
+  const handleChange = event => {
     const { value } = event.target;
-    this.setState({ query: value });
+    setQuery(value);
   };
 
-  onSubmit = event => {
+  const onSubmit = event => {
     event.preventDefault();
-    this.props.handleSubmit(this.state.query);
+    handleSubmit(query);
   };
 
-  render() {
-    return (
-      <>
-        <header className={css.Searchbar}>
-          <form className={css.SearchForm} onSubmit={this.onSubmit}>
-            <button type="submit" className="SearcFormButton">
-              <span className="button-label">Search</span>
-            </button>
+  return (
+    <>
+      <header className={css.Searchbar}>
+        <form className={css.SearchForm} onSubmit={onSubmit}>
+          <button type="submit" className="SearcFormButton">
+            <span className="button-label">Search</span>
+          </button>
 
-            <input
-              className={css.SearchFormInput}
-              onChange={this.handleChange}
-              value={this.state.query.trim()}
-              type="text"
-              autoComplete="off"
-              autoFocus
-              placeholder="Search images and photos"
-            />
-          </form>
-        </header>
-      </>
-    );
-  }
-}
+          <input
+            className={css.SearchFormInput}
+            onChange={handleChange}
+            value={query.trim()}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    </>
+  );
+};
+
 Searchbar.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
 };
+
+// export class Searchbar extends Component {
+//   state = {
+//     query: '',
+//   };
+
+// handleChange = event => {
+//   const { value } = event.target;
+//   this.setState({ query: value });
+// };
+
+// onSubmit = event => {
+//   event.preventDefault();
+//   this.props.handleSubmit(this.state.query);
+// };
+
+//   render() {
+// return (
+//   <>
+//     <header className={css.Searchbar}>
+//       <form className={css.SearchForm} onSubmit={this.onSubmit}>
+//         <button type="submit" className="SearcFormButton">
+//           <span className="button-label">Search</span>
+//         </button>
+
+//         <input
+//           className={css.SearchFormInput}
+//           onChange={this.handleChange}
+//           value={this.state.query.trim()}
+//           type="text"
+//           autoComplete="off"
+//           autoFocus
+//           placeholder="Search images and photos"
+//         />
+//       </form>
+//     </header>
+//   </>
+// );
+//   }
+// }
+// Searchbar.propTypes = {
+//   handleSubmit: PropTypes.func.isRequired,
+// };
