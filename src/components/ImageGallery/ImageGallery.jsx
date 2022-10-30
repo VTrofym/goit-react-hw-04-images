ec778465cba3072c88e8eee970a49811f63f7054
VTrofym <v.trof@icloud.com>
2022-10-30 14:08:11 +0200

    finished task

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,62 +1,112 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Button } from 'components/Button/Button';
 import { Loader } from '../Loader';
 import { getGalleryService } from 'Helpers/Api';
 import { ImageGalleryItem } from 'components/ImageGalleryItem';
 import css from '../ImageGallery/ImageGallery.module.css';
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 
-export class ImageGallery extends Component {
-  state = {
-    images: [],
-    page: 1,
-    isLoadind: false,
+export const ImageGallery = ({ toggleModal, query }) => {
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsloading] = useState(false);
+
+  useEffect(() => {
+    const getImage = async () => {
+      setIsloading(true);
+      const data = await getGalleryService(query);
+      setImages(data.hits);
+      setIsloading(false);
+    };
+    getImage();
+  }, [query]);
+
+  useEffect(() => {
+    if (page === 1) return;
+    const getData = async () => {
+      setIsloading(true);
+      const data = await getGalleryService(query, page);
+      setImages([...images, ...data.hits]);
+      setIsloading(false);
+    };
+    getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page]);
+
+  const loadMore = () => {
+    setPage(page + 1);
   };
 
-  async componentDidUpdate(prevProps, prevState) {
-    if (prevProps.query !== this.props.query && this.props.query !== '') {
-      this.setState({ isLoadind: true });
-      const data = await getGalleryService(this.props.query);
-      this.setState({
-        images: data.hits,
-        page: 1,
-        isLoadind: false,
-      });
-    }
-    if (prevState.page !== this.state.page && this.state.page !== 1) {
-      this.setState({ isLoadind: true });
-      const data = await getGalleryService(this.props.query, this.state.page);
+  return (
+    <>
+      {isLoading && <Loader />}
+      <ul className={css.ImageGallery}>
+        {images.map(image => (
+          <ImageGalleryItem
+            key={image.id}
+            toggleModal={toggleModal}
+            url={image.webformatURL}
+            largeImage={image.largeImageURL}
+          />
+        ))}
+      </ul>
+      <Button loadMore={loadMore} />
+    </>
+  );
+};
 
-      this.setState(prev => ({
-        images: [...prev.images, ...data.hits],
-        isLoadind: false,
-      }));
-    }
-  }
+// export class ImageGallery extends Component {
+//   state = {
+//     images: [],
+//     page: 1,
+//     isLoadind: false,
+//   };
 
-  loadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
-  };
+// async componentDidUpdate(prevProps, prevState) {
+//   if (prevProps.query !== this.props.query && this.props.query !== '') {
+//     this.setState({ isLoadind: true });
+//     const data = await getGalleryService(this.props.query);
+//     this.setState({
+//       images: data.hits,
+//       page: 1,
+//       isLoadind: false,
+//     });
+//   }
+//   if (prevState.page !== this.state.page && this.state.page !== 1) {
+//     this.setState({ isLoadind: true });
+//     const data = await getGalleryService(this.props.query, this.state.page);
 
-  render() {
-    return (
-      <>
-        {this.state.isLoading && <Loader />}
-        <ul className={css.ImageGallery}>
-          {this.state.images.map(image => (
-            <ImageGalleryItem
-              key={image.id}
-              toggleModal={this.props.toggleModal}
-              url={image.webformatURL}
-              largeImage={image.largeImageURL}
-            />
-          ))}
-        </ul>
-        <Button loadMore={this.loadMore} />
-      </>
-    );
-  }
-}
+//     this.setState(prev => ({
+//       images: [...prev.images, ...data.hits],
+//       isLoadind: false,
+//     }));
+//   }
+// }
+
+// loadMore = () => {
+//   this.setState(prevState => ({ page: prevState.page + 1 }));
+// };
+
+//   render() {
+// return (
+//   <>
+//     {this.state.isLoading && <Loader />}
+//     <ul className={css.ImageGallery}>
+//       {this.state.images.map(image => (
+//         <ImageGalleryItem
+//           key={image.id}
+//           toggleModal={this.props.toggleModal}
+//           url={image.webformatURL}
+//           largeImage={image.largeImageURL}
+//         />
+//       ))}
+//     </ul>
+//     <Button loadMore={this.loadMore} />
+//   </>
+// );
+//   }
+// }
 
 ImageGallery.propTypes = {
   query: PropTypes.string.isRequired,
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,38 +1,72 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import css from './Modal.module.css';
 
-export class Modal extends Component {
-  componentDidMount() {
-    document.addEventListener('keydown', this.keyClose);
-  }
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.keyClose);
-  }
+export const Modal = ({ onClose, largeImageURL }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', keyClose);
+    return () => {
+      window.removeEventListener('keydown', keyClose);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  keyClose = e => {
+  function keyClose(e) {
     if (e.key === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
-  };
-
-  overlayClick = e => {
-    if (e.target === e.currentTarget) {
-      this.props.onClose();
-    }
-  };
-
-  render() {
-    return (
-      <div onClick={this.overlayClick} className={css.Overlay}>
-        <div className={css.Modal}>
-          <img src={this.props.largeImageURL} alt="" />
-        </div>
-      </div>
-    );
   }
-}
+
+  const overlayClick = e => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
+  return (
+    <div onClick={overlayClick} className={css.Overlay}>
+      <div className={css.Modal}>
+        <img src={largeImageURL} alt="" />
+      </div>
+    </div>
+  );
+};
 
 Modal.propTypes = {
   largeImageURL: PropTypes.string.isRequired,
 };
+
+// export class Modal extends Component {
+// componentDidMount() {
+//   document.addEventListener('keydown', this.keyClose);
+// }
+//   componentWillUnmount() {
+//     document.removeEventListener('keydown', this.keyClose);
+//   }
+
+// keyClose = e => {
+//   if (e.key === 'Escape') {
+//     this.props.onClose();
+//   }
+// };
+
+// overlayClick = e => {
+//   if (e.target === e.currentTarget) {
+//     this.props.onClose();
+//   }
+// };
+
+//   render() {
+// return (
+//   <div onClick={this.overlayClick} className={css.Overlay}>
+//     <div className={css.Modal}>
+//       <img src={this.props.largeImageURL} alt="" />
+//     </div>
+//   </div>
+// );
+//   }
+// }
+
+// Modal.propTypes = {
+//   largeImageURL: PropTypes.string.isRequired,
+// };
